78570b420ea0dbc5047e57307b1634b90483344e
KosmosoJ <darko.donie3@gmail.com>
2024-07-18 14:32:09 +0500

    add cities db

--- app/routes/weather.py
+++ app/routes/weather.py
@@ -1,13 +1,30 @@
 from fastapi import APIRouter
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
+import json 
+from cachetools import TTLCache
+
 
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter()
+cache = TTLCache(maxsize=100, ttl=300)
+
+async def get_cities_info():
+    with open('database/cities_info.json', encoding='utf-8') as file:
+        data = json.load(file)
+    cities_data = []
+    for key in data['city']:
+        cities_data.append(key['name']) 
+    cache['cities'] = cities_data
+    return cities_data
 
 
 @router.get("/weather/")
 async def weather_index(request: Request):
+    if 'cities' in cache:
+        cities_data = cache['cities']
+    else:
+        cities_data = await get_cities_info()
     print(request.client.host)
-    return templates.TemplateResponse(request=request, name="weather.html")
+    return templates.TemplateResponse(request=request, name="weather.html", context={'cities':cities_data})
